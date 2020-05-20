29cfed7c86e5102ddc96d72292a706762b5c31c7
huzbadri53 <badrihuzaifa52@gmail.com>
2020-05-20 10:47:45 +0000

    Exploring Python on my own

--- get-quote.py
+++ get-quote.py
@@ -7,8 +7,9 @@ def cool():
   f.close()
   last = len(quotes) - 1
   rnd = random.randint(0, last)
+  rnd1 = random.randint(0, last)
 
-  print(quotes[rnd])
+  print(quotes[rnd],quotes[rnd1])
 
 if __name__== "__main__":
   cool()
