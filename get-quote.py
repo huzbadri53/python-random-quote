52eb733199065addec379216b618e006535808a9
huzbadri53 <badrihuzaifa52@gmail.com>
2020-05-20 11:47:52 +0000

    Learning on own

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,18 @@
 import random
 def cool():
   #print("Keep it logically awesome.")
-
+  f = open("quotes.txt","w")
+  for i in range(5):
+    f.write("This is Line number %d\r\n" % (i+1))
+  f.close()
   f = open("quotes.txt")
   quotes = f.readlines()
-  f.close()
+  #f = open("quotes.txt","r")
+  #if f.mode == "r":
+  #  quotes = f.read()
+  #print (quotes)
+  #quotes = f.read()
+  #f.close()
   last = len(quotes) - 1
   rnd = random.randint(0, last)
   rnd1 = random.randint(0, last)
